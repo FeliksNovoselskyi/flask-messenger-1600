d6c775805bba87f7af324238c2447eea1d5b2d91
WorldIT-academy <worldit_academy@worldit.academy>
2026-09-13 17:47:22 +0300

    Start authorization

--- app/urls.py
+++ app/urls.py
@@ -2,6 +2,7 @@ from .settings import main_app
 from chat_app.views import render_reg
 # Импортировать функции отображения
 from chat_app.views import render_chat
+from chat_app.views import render_auth
 from chat_app.app import chat_app_blueprint
 
 
@@ -14,6 +15,14 @@ main_app.add_url_rule(
 main_app.add_url_rule(
     rule = "/reg",
     view_func = render_reg,
+    methods = ["GET", "POST"]
+)
+
+# Егор Столяров
+# Указать адрес страніци авторізаціі
+main_app.add_url_rule(
+    rule = "/auth",
+    view_func = render_auth,
     methods = ["GET", "POST"]
 )
 
--- chat_app/views.py
+++ chat_app/views.py
@@ -1,12 +1,11 @@
 import flask
 from chat_app.models import User
+import flask_login
 
 from app.db import DATABASE
 import werkzeug
 
-# Werkzeug - библиотека для работы с хешами
 
-# DATABASE db.py
 
 def render_chat():
     return flask.render_template("chat.html")
@@ -45,3 +44,32 @@ def render_reg():
 
 # хеш невозможно расшифровать 
 # все нежные данные в БД - хешируем
+
+# Паша
+# Создать функцію отображенія шаблона auth.html
+
+def render_auth():
+    if flask.request.method == "POST":
+        email = flask.request.form.get("email")
+        password = flask.request.form.get("password")
+        
+        if email and password:
+            user = User.query.filter_by(email = email)
+            
+            # SELECT user.id AS user_id, user.email AS user_email, user.password AS user_password 
+            # FROM user 
+            # WHERE user.email = ?
+            print("\n", user)
+            
+            
+            if user:
+                # Он отправляет запрос, возвращает объект пользователя
+                user = user.scalar()
+                
+                # <User 1> felixdnbox@gmail.com
+                print(user, user.email)
+
+                flask_login.login_user(user=user)
+            
+            
+    return flask.render_template("auth.html")
